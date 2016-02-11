81519d3e4d6939f8b18183ab564e07fa0265a6de
Andrus Adamchik <andrus@objectstyle.com>
2016-02-11 17:21:10 +0300

    example app

--- bootique-bom-example-rest/src/main/java/com/nhl/bootique/bom/HelloResource.java
+++ bootique-bom-example-rest/src/main/java/com/nhl/bootique/bom/HelloResource.java
@@ -1,13 +1,26 @@
 package com.nhl.bootique.bom;
 
+import static java.util.stream.Collectors.joining;
+
+import java.util.Arrays;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
+import com.google.inject.Inject;
+import com.nhl.bootique.annotation.Args;
+
 @Path("/")
 public class HelloResource {
+	
+	@Inject
+	@Args
+	private String[] args;
 
 	@GET
 	public String hello() {
-		return "Hello, world!";
+		
+		String allArgs = Arrays.asList(args).stream().collect(joining(" "));
+		return "Hello, world! The app was started with the following arguments: " + allArgs;
 	}
 }
