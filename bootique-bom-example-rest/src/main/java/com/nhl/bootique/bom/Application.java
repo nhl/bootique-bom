2075fabe25dfd64d9a70a4ba239dd29591bc53c1
Andrus Adamchik <andrus@objectstyle.com>
2016-02-12 17:52:31 +0300

    renaming main class

--- bootique-bom-example-rest/src/main/java/com/nhl/bootique/bom/Application.java
+++ bootique-bom-example-rest/src/main/java/com/nhl/bootique/bom/Application.java
@@ -3,7 +3,7 @@ package com.nhl.bootique.bom;
 import com.nhl.bootique.Bootique;
 import com.nhl.bootique.jersey.JerseyModule;
 
-public class Main {
+public class Application {
 
 	public static void main(String[] args) {
 		JerseyModule jersey = JerseyModule.builder().packageRoot(HelloResource.class).build();
