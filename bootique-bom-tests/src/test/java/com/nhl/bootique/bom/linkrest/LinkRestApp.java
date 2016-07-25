4a039e5b46dae4ad449b1b0e1ac35a6b310b4a92
Andrus Adamchik <andrus@objectstyle.com>
2016-07-25 17:18:49 +0300

    Cayenne upgarde requires some tweaks per #12

--- bootique-bom-tests/src/test/java/com/nhl/bootique/bom/linkrest/LinkRestApp.java
+++ bootique-bom-tests/src/test/java/com/nhl/bootique/bom/linkrest/LinkRestApp.java
@@ -1,33 +1,27 @@
 package com.nhl.bootique.bom.linkrest;
 
-import static org.mockito.Mockito.mock;
-
-import java.util.function.Consumer;
-
-import com.google.inject.Module;
 import com.nhl.bootique.Bootique;
 import com.nhl.bootique.bom.linkrest.r1.LrResource1;
 import com.nhl.bootique.cayenne.CayenneModule;
-import com.nhl.bootique.jdbc.DataSourceFactory;
+import com.nhl.bootique.jdbc.JdbcModule;
 import com.nhl.bootique.jersey.JerseyModule;
 import com.nhl.bootique.jetty.test.junit.JettyTestFactory;
 import com.nhl.bootique.linkrest.LinkRestModule;
 
+import java.util.function.Consumer;
+
 public class LinkRestApp extends JettyTestFactory {
 
-	@Override
-	public Builder newRuntime() {
+    @Override
+    public Builder newRuntime() {
 
-		Module cayenne = CayenneModule.builder().noConfig().build();
-		Module jdbc = b -> b.bind(DataSourceFactory.class).toInstance(mock(DataSourceFactory.class));
+        Consumer<Bootique> config = (bootique) -> {
+            bootique.modules(JerseyModule.class, LinkRestModule.class, CayenneModule.class, JdbcModule.class).module((binder) -> {
+                JerseyModule.contributeResources(binder).addBinding().to(LrResource1.class);
+            });
+        };
 
-		Consumer<Bootique> config = (bootique) -> {
-			bootique.modules(JerseyModule.class, LinkRestModule.class).modules(cayenne, jdbc, (binder) -> {
-				JerseyModule.contributeResources(binder).addBinding().to(LrResource1.class);
-			});
-		};
-
-		return super.newRuntime().configurator(config);
-	}
+        return super.newRuntime().configurator(config);
+    }
 
 }
